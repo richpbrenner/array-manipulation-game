7af998ece20d3a56cc3c51c2967ed37fb0644c7c
Richard P Brenner <richpbrenner@gmail.com>
2017-06-01 15:39:20 -0400

    generate number list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    const numberList = this.generateNumberList();
+    this.state = {
+      numberList : numberList,
+    };
+    this.manipulationMethods = {
+    };
+  }
+
+  generateNumberList = () => {
+    let numberList = []
+    for (let i = 0; i < 5; i++) {
+      numberList.push(Math.floor(Math.random()*10));
+    }
+    return numberList;
+  }
+
+  manipulateList = (numberOfMethods) => {
+    let newNumberList = [...this.state.numberList];
+
+  }
+
+  chooseManipulationMethod = () => {
+    //reverse, slice, increment/decrement all, multiply all, square all
+    //sort low to high, sort high to low
+
+  }
+
   render() {
     return (
       <div className="App">
+        {this.state.numberList}
       </div>
     );
   }
